4d33b877f248d5ed2e31a7630ea06cabb592e7df
Daniela <dany180604acosta@gmail.com>
2024-12-16 17:39:17 -0500

    Create preguntas.pl

---
+++ preguntas.pl
@@ -0,0 +1,6 @@
+% % Las preguntas
+prop(1,'el motor esta trancado',p).
+prop(2, 'el motor arranca',p).
+prop(3, 'el motor se calienta',p).
+prop(4, 'el motor gira a baja veocidad',p).
+prop(5, 'el motor hace ruido', p).
